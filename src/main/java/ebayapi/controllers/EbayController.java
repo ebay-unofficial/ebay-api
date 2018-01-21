0f29a676349cd17c4e0d065eea641af10bd63eae
Tim Kilian <timkilian51@gmail.com>
2018-01-21 04:35:35 +0100

    Add cors

--- src/main/java/ebayapi/controllers/EbayController.java
+++ src/main/java/ebayapi/controllers/EbayController.java
@@ -9,13 +9,11 @@ import ebayapi.utils.EbayOrderType;
 import ebayapi.utils.EbaySearchRequest;
 import ebayapi.services.EbaySearchParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class EbayController {
 
     @Autowired
