d8044c8fc523c98b771112100efabe40cd683f55
Leandro Baltazar <baltazar@adobe.com>
2018-11-18 20:33:02 +0100

    Add logging for ebay requests

--- src/main/java/ebayapi/services/EbayHttpService.java
+++ src/main/java/ebayapi/services/EbayHttpService.java
@@ -2,9 +2,12 @@ package ebayapi.services;
 
 import ebayapi.utils.EbaySearchRequest;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -12,6 +15,7 @@ import java.io.IOException;
 @Service
 public class EbayHttpService {
 
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     private String baseUrl = "https://www.ebay.de";
     private String lastRequest = "";
@@ -24,7 +28,11 @@ public class EbayHttpService {
         HttpClient httpClient = HttpClientBuilder.create().build();
         try {
             lastRequest = url;
-            return IOUtils.toString(httpClient.execute(new HttpGet(baseUrl + url)).getEntity().getContent());
+            String fullUrl = baseUrl + url;
+            HttpResponse response = httpClient.execute(new HttpGet(fullUrl));
+            log.info("Request: status: {}, url: {}", response.getStatusLine().getStatusCode(), fullUrl);
+
+            return IOUtils.toString(response.getEntity().getContent());
         } catch (IOException e) {
             e.printStackTrace();
         }
